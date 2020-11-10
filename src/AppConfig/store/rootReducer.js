5a90f9534afcbf609d3c8db515dfd1b3d4e905f7
Dhananjay Kumar <dkp12cs12@gmail.com>
2020-11-10 10:10:29 +0530

    --> error fixes

--- src/AppConfig/store/rootReducer.js
+++ src/AppConfig/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import toaster from '../MessageToaster/reducer'
 import appCommon from './index'
+import project from '../../Components/Project/reducers'
 
 const appReducer = combineReducers({
     toaster,
-    appCommon
+    appCommon,
+    project
 })
 
 const rootReducer = (state, action) => {
