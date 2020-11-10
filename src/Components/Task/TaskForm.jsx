72d1a32e6994d240c54796dc1a36a0a0ae560536
Dhananjay Kumar <dkp12cs12@gmail.com>
2020-11-10 12:08:39 +0530

    --> fetch developer api.

--- src/Components/Task/TaskForm.jsx
+++ src/Components/Task/TaskForm.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "../Shared/TextField";
 import Selector from "../Shared/Selector";
-import { Roles } from "../../AppConfig/constants";
 import "../SignUp/sigin.styles.css";
+import API from "../../AppConfig/Api";
+import { API_CONSTANT } from "../../AppConfig/APIConstants";
+import { useDispatch } from "react-redux";
+import toaster from "../../AppConfig/MessageToaster/actions";
 
 /**
  * Used to show the basic SignUp of the app.
- @returns {*}
- @typedef Heading(string) value to be shown as a header in dialog 
+ @returns {*} 
  @param {{
-    heading Heading
  }} props
 */
 
@@ -19,10 +20,28 @@ const TaskForm = () => {
     description: "",
     assignedManager: 0,
   });
+  const [developers, setDevelopers] = useState([])
+  const dispatch = useDispatch()
 
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
+
+  useEffect(() => {
+    API.get(API_CONSTANT.getAllDevelopers)
+      .then(result => {
+        if(result && result.length)
+          setDevelopers(result.map(developer => {
+            return {
+              value: Number(developer.id),
+              label: developer.userName
+            }
+          }))
+      })
+      .catch(error => {
+        dispatch(toaster.error(error.message))
+      })
+  },[dispatch])
 
   return (
     <div className="signUpBody">
@@ -42,7 +61,7 @@ const TaskForm = () => {
         <Selector
           label="Assign Developer"
           onChange={handleChange("assignedDeveloper")}
-          dataProvider={Roles}
+          dataProvider={developers}
           width="100%"
           value={values.assignedDeveloper}
         />
